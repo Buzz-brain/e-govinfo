45612a6aed03c09fcdd375d1a7a98825da6ebfe1
Buzz-brain <chinomsochristian03@gmail.com>
2025-10-06 15:02:25 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(helmet());
 const allowedOrigins = [
-  "https://e-govinfo-app--31vyvdkj9h.expo.app",
+  "https://e-govinfo-app--uc5qf9b7mb.expo.app",
   "http://localhost:8081",
   "http://localhost:3000",
   "http://localhost:5000",
